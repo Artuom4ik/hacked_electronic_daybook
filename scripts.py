ffa0eefa0052c0749bd4fad057138805d15edc25
Artuom4ik <masterskayaigr123@gmail.com>
2023-04-12 20:03:01 +0300

    Написал список похвал.

--- scripts.py
+++ scripts.py
@@ -1,7 +1,41 @@
 from datacenter.models import Schoolkid, Subject 
 from datacenter.models import Chastisement, Mark
 from datacenter.models import Lesson, Commendation
-from random import randint
+from random import randint, choice
+
+
+COMENDATIONS = [
+    "Молодец!",
+    "Отлично!",
+    "Хорошо!",
+    "Гораздо лучше, чем я ожидал!",
+    "Ты меня приятно удивил!",
+    "Великолепно!",
+    "Прекрасно!",
+    "Ты меня очень обрадовал!",
+    "Именно этого я давно ждал от тебя!",
+    "Сказано здорово – просто и ясно!",
+    "Ты, как всегда, точен!",
+    "Очень хороший ответ!",
+    "Талантливо!",
+    "Ты сегодня прыгнул выше головы!",
+    "Я поражен!",
+    "Уже существенно лучше!",
+    "Потрясающе!",
+    "Замечательно!",
+    "Прекрасное начало!",
+    "Так держать!",
+    "Ты на верном пути!",
+    "Здорово!",
+    "Это как раз то, что нужно!",
+    "Я тобой горжусь!",
+    "С каждым разом у тебя получается всё лучше!",
+    "Мы с тобой не зря поработали!",
+    "Я вижу, как ты стараешься!",
+    "Ты растешь над собой!",
+    "Ты многое сделал, я это вижу!",
+    "Теперь у тебя точно все получится!",
+    ]
 
 
 def fix_marks(schoolkid):
@@ -19,11 +53,11 @@ def create_commendation(schoolkid, title):
     schoolkid_lessons = Lesson.objects.filter(year_of_study=schoolkid.year_of_study, group_letter=schoolkid.group_letter, subject=subject[0])
     for lesson in schoolkid_lessons:
         Commendation.objects.create(
-            schoolkid=schoolkid, 
-            teacher=lesson.teacher, 
-            subject=lesson.subject, 
-            created=lesson.date, 
-            text="Хвалю!")
+            schoolkid=schoolkid,
+            teacher=lesson.teacher,
+            subject=lesson.subject,
+            created=lesson.date,
+            text=choise(COMENDATIONS))
 
 
 if __name__ == "__main__":
